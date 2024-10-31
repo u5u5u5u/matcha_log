3fed11ac28bbee48af0bab5e05d98939f856a0f5
U5 <kyutech.joko@gmail.com>
2024-10-31 23:28:56 +0900

    ログインページにshadcnui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,17 @@
 import { login, signup } from "./actions";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function LoginPage() {
   return (
     <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
+      <Label htmlFor="email">Email:</Label>
+      <Input id="email" name="email" type="email" required />
+      <Label htmlFor="password">Password:</Label>
+      <Input id="password" name="password" type="password" required />
+      <Button formAction={login}>Log in</Button>
+      <Button formAction={signup}>Sign up</Button>
     </form>
   );
 }
