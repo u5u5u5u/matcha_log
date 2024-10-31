e62a37123fd85839d93cb696b1dc874a2baa3e54
U5 <kyutech.joko@gmail.com>
2024-10-31 22:07:15 +0900

    スマホ特化の幅に

--- src/app/account/account-form.tsx
+++ src/app/account/account-form.tsx
@@ -80,7 +80,6 @@ export default function AccountForm({ user }: { user: User | null }) {
 
   return (
     <div className="form-widget">
-      {/* ... */}
       <Avatar
         uid={user?.id ?? null}
         url={avatar_url}
