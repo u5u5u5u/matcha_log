8c6b5bc2969f007e438ebbd459bf9f4d3e9b2430
U5 <kyutech.joko@gmail.com>
2024-11-02 00:17:39 +0900

    float buttonの色変更

--- src/components/ui/floatButton.tsx
+++ src/components/ui/floatButton.tsx
@@ -4,7 +4,7 @@ const FloatButton = () => {
   return (
     <Link
       href="/registration"
-      className="fixed right-5 bottom-5 z-[9999] block bg-primary-400 text-white w-12 h-12 text-center leading-[50px] text-2xl rounded-full shadow-md hover:bg-[#FF6F47]"
+      className="fixed right-5 bottom-5 z-[9999] block bg-primary-400 text-white w-12 h-12 text-center leading-[50px] text-2xl rounded-full shadow-md hover:bg-primary-600"
     >
       ＋
     </Link>
