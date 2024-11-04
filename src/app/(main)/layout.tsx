f2a0de013e08a21ed2d85a22c06ef8447f58e73b
U5 <kyutech.joko@gmail.com>
2024-11-04 16:37:29 +0900

    ヘッダーのタイトルを動的パスに対応

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,10 +3,15 @@
 import { usePathname } from "next/navigation";
 import Header from "@/components/Header/Header";
 
-const PageTitle: { [key: string]: string } = {
-  "/home": "Home",
-  "/matcha/registration": "新規登録",
-};
+const PageTitleMap = [
+  { path: /^\/home$/, title: "Home" },
+  { path: /^\/matcha$/, title: "まっちゃ" },
+  { path: /^\/matcha\/[^/]+$/, title: "詳細" },
+  { path: /^\/matcha\/[^/]+\/edit$/, title: "編集" },
+  { path: /^\/matcha\/registration$/, title: "新規登録" },
+  { path: /^\/profile$/, title: "プロフィール" },
+  { path: /^\/ranking$/, title: "ランキング" },
+];
 
 export default function RootLayout({
   children,
@@ -15,7 +20,10 @@ export default function RootLayout({
 }) {
   const pathname = usePathname();
   const showBackButton = pathname !== "/home";
-  const pageTitle = PageTitle[pathname] || "";
+  const pageTitle =
+    PageTitleMap.find((item) => item.path.test(pathname))?.title || "";
+
+  console.log("pathname", pathname);
 
   return (
     <>
