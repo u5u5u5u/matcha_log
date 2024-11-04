4c2b2af0a73138a7ce9cee516118c982aee80321
U5 <kyutech.joko@gmail.com>
2024-11-04 22:19:27 +0900

    詳細ページの要素だけ

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
       <ul>
         <li>
           <Link href="/matcha">Matcha</Link>
--- src/app/(main)/matcha/[id]/page.tsx
+++ src/app/(main)/matcha/[id]/page.tsx
@@ -1,8 +1,35 @@
+import Image from "next/image";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+
 const MatchaDetail = () => {
   return (
-    <div>
-      <h1>Matcha Detail</h1>
-    </div>
+    <>
+      <div className="flex justify-around">
+        <div className="w-[200px]">
+          <AspectRatio ratio={1 / 1}>
+            <Image
+              src="/dummyMatcha.jpg"
+              fill
+              alt="抹茶かき氷"
+              className="rounded-md object-cover"
+            />
+          </AspectRatio>
+        </div>
+
+        <div>
+          <p>抹茶かき氷</p>
+          <p>¥1,000</p>
+          <p>AZUMIYA</p>
+          <p>2021/07/01</p>
+          <p>東京都</p>
+        </div>
+      </div>
+      <div>
+        <p>苦さ: 5</p>
+        <p>甘さ: 7</p>
+        <p>濃さ: 8</p>
+      </div>
+    </>
   );
 };
 
