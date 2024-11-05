4e7d12b108c0c0b233cee2e0b56bffd9d47be1c1
U5 <kyutech.joko@gmail.com>
2024-11-05 10:41:28 +0900

    詳細ページある程度のレイアウト

--- src/app/(main)/matcha/[id]/page.tsx
+++ src/app/(main)/matcha/[id]/page.tsx
@@ -4,30 +4,48 @@ import { AspectRatio } from "@/components/ui/aspect-ratio";
 const MatchaDetail = () => {
   return (
     <>
-      <div className="flex justify-around">
-        <div className="w-[200px]">
+      <div className="flex flex-col items-center">
+        <div className="w-full max-w-[428px]">
           <AspectRatio ratio={1 / 1}>
             <Image
               src="/dummyMatcha.jpg"
               fill
               alt="抹茶かき氷"
-              className="rounded-md object-cover"
+              className="object-cover"
             />
           </AspectRatio>
         </div>
 
-        <div>
-          <p>抹茶かき氷</p>
-          <p>¥1,000</p>
-          <p>AZUMIYA</p>
-          <p>2021/07/01</p>
-          <p>東京都</p>
+        <div className="flex flex-col w-full mt-1">
+          <p className="text-right text-sm">2021年7月1日</p>
+          <div className="w-full">
+            <div>
+              <h3 className="text-xl">抹茶かき氷</h3>
+              <p>スイーツ</p>
+            </div>
+            <p>¥1,000</p>
+            <div className="flex">
+              <p>AZUMIYA</p>
+              <p>東京都</p>
+            </div>
+          </div>
+          <div className="w-2/3">
+            <ul className="flex justify-between">
+              <li className="text-center">
+                <p>苦さ</p>
+                <p>5</p>
+              </li>
+              <li className="text-center">
+                <p>甘さ</p>
+                <p>7</p>
+              </li>
+              <li className="text-center">
+                <p>濃さ</p>
+                <p>3</p>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div>
-        <p>苦さ: 5</p>
-        <p>甘さ: 7</p>
-        <p>濃さ: 8</p>
       </div>
     </>
   );
