ca74c9fb3d8ecbd2d5e0c77f9e68d9f7799d57d9
U5 <kyutech.joko@gmail.com>
2024-11-09 15:05:19 +0900

    tailwindわからん

--- src/app/(main)/ranking/page.tsx
+++ src/app/(main)/ranking/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+import { Card } from "@/components/ui/card";
 import {
   Table,
   TableBody,
@@ -24,44 +25,46 @@ import { dummyShop } from "@/utils/dummy/shop";
 
 const Ranking = () => {
   return (
-    <>
-      <Table className="h-[85vh]">
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[60px]">順位</TableHead>
-            <TableHead className="w-[150px]">名前</TableHead>
-            <TableHead className="w-[">都道府県</TableHead>
-            <TableHead className="text-right">得点</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {dummyMatcha.map((matcha, index) => (
-            <TableRow key={matcha.id}>
-              <TableCell className="font-medium text-center">
-                {index + 1}
-              </TableCell>
-
-              <TableCell className="font-medium">
-                <Link href={`/matcha/${matcha.id}`}>{matcha.name}</Link>
-              </TableCell>
-
-              <TableCell>
-                {
-                  dummyPrefecture.find(
-                    (p) =>
-                      p.id ===
-                      dummyShop.find((s) => s.id === matcha.shop_id)
-                        ?.prefecture_id
-                  )?.name
-                }
-              </TableCell>
-              <TableCell className="text-right">{matcha.price}</TableCell>
+    <Card className="h-[88vh] py-2 px-4 my-4">
+      <div className="h-[90%]">
+        <Table className="h-full">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[17%]">順位</TableHead>
+              <TableHead className="w-[40%]">名前</TableHead>
+              <TableHead className="w-[25%]">都道府県</TableHead>
+              <TableHead className="w-[18%] text-right">得点</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody className="">
+            {dummyMatcha.map((matcha, index) => (
+              <TableRow key={matcha.id}>
+                <TableCell className="font-medium text-center">
+                  {index + 1}
+                </TableCell>
 
-      <Pagination className="h-[8vh]">
+                <TableCell className="font-medium">
+                  <Link href={`/matcha/${matcha.id}`}>{matcha.name}</Link>
+                </TableCell>
+
+                <TableCell>
+                  {
+                    dummyPrefecture.find(
+                      (p) =>
+                        p.id ===
+                        dummyShop.find((s) => s.id === matcha.shop_id)
+                          ?.prefecture_id
+                    )?.name
+                  }
+                </TableCell>
+                <TableCell className="text-right">{matcha.price}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+
+      <Pagination className="">
         <PaginationContent>
           <PaginationItem>
             <PaginationPrevious href="#" />
@@ -70,9 +73,7 @@ const Ranking = () => {
             <PaginationLink href="#">1</PaginationLink>
           </PaginationItem>
           <PaginationItem>
-            <PaginationLink href="#">
-              2
-            </PaginationLink>
+            <PaginationLink href="#">2</PaginationLink>
           </PaginationItem>
           <PaginationItem>
             <PaginationLink href="#">3</PaginationLink>
@@ -85,7 +86,7 @@ const Ranking = () => {
           </PaginationItem>
         </PaginationContent>
       </Pagination>
-    </>
+    </Card>
   );
 };
 
