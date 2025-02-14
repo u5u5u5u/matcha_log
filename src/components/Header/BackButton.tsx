ce53dfdeefb49f867c7c20935f78bda0cff53c90
U5 <kyutech.joko@gmail.com>
2025-02-14 22:32:07 +0900

    chage:back button変更

--- src/components/Header/BackButton.tsx
+++ src/components/Header/BackButton.tsx
@@ -1,12 +1,17 @@
 "use client";
+
 import { useRouter } from "next/navigation";
+import { ArrowLeft } from "lucide-react";
 
 const BackButton = () => {
   const router = useRouter();
 
   return (
-    <button onClick={() => router.back()} className="fixed left-4 top-4 text-white font-bold px-3">
-      ← 戻る
+    <button
+      onClick={() => router.back()}
+      className="fixed left-4 text-white font-bold px-3"
+    >
+      <ArrowLeft size={24} color="#000" />
     </button>
   );
 };
