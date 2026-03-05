efdc6d82a8b9a6da6a5fb34f20b0362d48d28fea
U5 <kyutech.joko@gmail.com>
2026-03-05 13:10:17 +0900

    fix: actionsエラー解消

--- src/app/(main)/post/new/page.tsx
+++ src/app/(main)/post/new/page.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
 import PostForm from "@/components/post/new/PostForm";
 import styles from "./page.module.scss";
 
 export default function PostNewPage() {
   return (
     <div className={styles.container}>
-      <PostForm />
+      <Suspense fallback={null}>
+        <PostForm />
+      </Suspense>
     </div>
   );
 }
